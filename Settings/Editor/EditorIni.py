e96d44d8785e4b0e2b9d60d002f96d1605696e71
Joshua Neff <jneffit@gmail.com>
2023-09-02 13:15:24 -0400

    fixing files

---
+++ Settings/Editor/EditorIni.py
@@ -0,0 +1,3 @@
+editor_settings = dict()
+
+editor_settings["name"] = "Team Neff Editor"
---
+++ UI/UX/Editor/Editor.py
@@ -0,0 +1,46 @@
+import sys
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QAction
+from PyQt5.QtGui import QIcon
+
+from Settings.Editor.EditorIni import editor_settings
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        mainMenu=self.menuBar()
+        fileMenu=mainMenu.addMenu('File')
+        editMenu=mainMenu.addMenu('Edit')
+        viewMenu=mainMenu.addMenu('View')
+        searchMenu=mainMenu.addMenu('Search')
+        toolsMenu=mainMenu.addMenu('Tools')
+        helpMenu=mainMenu.addMenu('Help')
+        exitButton=QAction(QIcon('exit24.png'), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        fileMenu.addAction(exitButton)
+        self.show()
+
+        self.setWindowTitle(editor_settings["name"])
+        #button = QPushButton(editor_settings["name"])
+
+        # Set the central widget of the Window.
+        #self.setCentralWidget(button)
+
+
+def start_editor():
+	app = QApplication(sys.argv)
+
+	window = MainWindow()
+	#window.show()
+
+	app.exec()
+	return app
+	
+if __name__ == "__main__":
+	start_editor()
