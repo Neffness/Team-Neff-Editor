c93d57fd04038afdfefbc31809519315ce6af531
Joshua Neff <jneffit@gmail.com>
2023-09-02 13:05:51 -0400

    fixing file

--- GameEngine.py
+++ GameEngine.py
@@ -1,1 +1,5 @@
-/data/data/com.termux/files/home/documents/Pydroid3/GameEngine.py
+import UI.UX.Editor.Editor as Editor
+from PyQt5.QtCore import QSize
+
+if __name__ == "__main__":
+	Editor.start_editor()
