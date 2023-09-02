a94428fbfb3b462155d69877e7dcc4902f174f7f
Joshua Neff <jneffit@gmail.com>
2023-09-02 15:24:10 -0400

    Updated gui to use pygame

--- UI/UX/Editor/Editor.py
+++ UI/UX/Editor/Editor.py
@@ -1,15 +1,31 @@
 import sys
 
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QAction, QWidget
+from PyQt5.QtGui import QImage, QPainter
 from PyQt5.QtGui import QIcon
+
+import pygame
 
 from Settings.Editor.EditorIni import editor_settings
 
+class ImageWidget(QWidget):
+    def __init__(self,surface,parent=None):
+        super(ImageWidget,self).__init__(parent)
+        w=surface.get_width()
+        h=surface.get_height()
+        self.data=surface.get_buffer().raw
+        self.image=QImage(self.data,w,h,QImage.Format_RGB32)
 
+    def paintEvent(self,event):
+        qp=QPainter()
+        qp.begin(self)
+        qp.drawImage(0,0,self.image)
+        qp.end()
+        
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, surface,parent=None):
         super().__init__()
         
         mainMenu=self.menuBar()
@@ -23,20 +39,23 @@ class MainWindow(QMainWindow):
         self.show()
 
         self.setWindowTitle(editor_settings["name"])
-        #button = QPushButton(editor_settings["name"])
-
-        # Set the central widget of the Window.
-        #self.setCentralWidget(button)
+        
+        self.setCentralWidget(ImageWidget(surface))
 
 
 def start_editor():
-	app = QApplication(sys.argv)
-
-	window = MainWindow()
-	#window.show()
-
-	app.exec()
-	return app
+	#app = QApplication(sys.argv)
+	#window = MainWindow()
+	#app.exec()
+	#return app
+	pygame.init()
+	s=pygame.Surface((640,480))
+	s.fill((64,128,192,224))
+	pygame.draw.circle(s,(255,255,255,255),(100,100),50)
+	app=QApplication(sys.argv)
+	w=MainWindow(s)
+	w.show()
+	app.exec_()
 	
 if __name__ == "__main__":
 	start_editor()
